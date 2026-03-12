305d9b0d8bfe151c883fc14470bbd50233488538
Larry <L.abbo@yahoo.com>
2026-03-12 14:56:01 +0200

    Delete app/upgrade/UpgradeClient.tsx

--- app/upgrade/UpgradeClient.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import UpgradeIntentTracker from "@/app/components/UpgradeIntentTracker";
-
-export default function UpgradeClient() {
-  return <UpgradeIntentTracker source="upgrade-page" />;
-}
