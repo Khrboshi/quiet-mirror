ca2eede18f0a7a03967a31cad1717d8fa645848c
Larry <L.abbo@yahoo.com>
2026-03-26 14:53:16 +0200

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,4 @@
-import { colors, branding } from './design-tokens';
+import { colors, branding } from '../design-tokens';
 import Link from "next/link";
 import { CONFIG } from "@/app/lib/config";
 
